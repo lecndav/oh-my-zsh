19c16773976a677c87bda9653fa12146822cbde3
David Lechner <David@davidmacbook.home>
2016-04-09 16:37:30 +0200

    upd

--- tools/install.sh
+++ tools/install.sh
@@ -75,8 +75,8 @@ main() {
 
   printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
   cp "$ZSH/templates/zshrc.zsh-template" "~/.zshrc"
-  sed "/^export \"ZSH\"=/ c\\
-  export \"ZSH\"=\"$ZSH\"
+  sed "/^export ZSH=/ c\\
+  export ZSH=\"$ZSH\"
   " "~/.zshrc" > "~/.zshrc-omztemp"
   mv -f "~/.zshrc-omztemp" "~/.zshrc"
 

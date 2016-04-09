e4debb6d1d1630efce127f4b51c49e5cc47cbe4e
David Lechner <d_lechner_94@gmx.at>
2016-04-09 16:26:08 +0200

    upd

--- tools/install.sh
+++ tools/install.sh
@@ -75,7 +75,7 @@ main() {
 
   printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
   cp "$ZSH/templates/zshrc.zsh-template" "~/.zshrc"
-  sed "/^export ZSH=/ c\\
+  sed "/^export \"ZSH\"=/ c\\
   export ZSH=\"$ZSH\"
   " ~/.zshrc > ~/.zshrc-omztemp
   mv -f ~/.zshrc-omztemp ~/.zshrc
